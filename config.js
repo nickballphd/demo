b5301337b1f39e422b7811de7cf8663c3afa65e0
nickballphd <39495054+nickballphd@users.noreply.github.com>
2022-02-14 17:01:14 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+//This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
+
+function gas_deployment_id(){return(
+'AKfycbyVnMp6LeO6J5YV4iL8vm9KmzdM9Rzx5oWGgS3R3E9XCGbWYl8CtuCAejYiWVne7RugHA'
+)}
